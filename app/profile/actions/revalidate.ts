b22196a24e61a6c1d671ef746bf9470b976b6b07
Sami-Nashawi <mohammadsaminashawi@gmail.com>
2024-08-15 09:03:15 +0000

    trying js

--- app/profile/actions/revalidate.ts
+++ app/profile/actions/revalidate.ts
@@ -5,7 +5,6 @@ import { redirect } from "next/navigation"
 
  
 export async function revalidate() {
-    new Promise((resolve) => setTimeout(resolve,5000));
     revalidatePath('/image','layout')
         redirect('/image')
 }
