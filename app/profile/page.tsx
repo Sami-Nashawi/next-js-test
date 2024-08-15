4dc04f5b30dbcad08cc7d5e05ab792f7c674a14e
Sami-Nashawi <mohammadsaminashawi@gmail.com>
2024-08-15 09:27:34 +0000

    test12

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,9 @@
-import RevalidateButton from "./revalidate-button";
+// import RevalidateButton from "./revalidate-button";
 
 const Profile = () => {
     return <>
     <h1>profile page</h1>
-    <RevalidateButton/>
+    {/* <RevalidateButton/> */}
     </>
 }
 
