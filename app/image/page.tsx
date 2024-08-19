299d74f7261975d2c5f0d15ff0421cd631cb85f5
Sami-Nashawi <mohammadsaminashawi@gmail.com>
2024-08-19 12:55:29 +0000

    test no store

--- app/image/page.tsx
+++ app/image/page.tsx
@@ -1,7 +1,10 @@
 import { Suspense } from "react";
 import Fsdf from "./f";
+import { unstable_noStore } from "next/cache";
 export const dynamic = 'force-dynamic';
 const RandomImage =  () => {
+    unstable_noStore()
+
     return <>
     <h1>image page</h1>
     <Suspense fallback={<p>Fetching Image...</p>}>
