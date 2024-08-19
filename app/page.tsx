9c319dcc891ae4c35797f911de0879bac8f871f6
Sami-Nashawi <mohammadsaminashawi@gmail.com>
2024-08-19 04:03:22 +0000

    test server actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
 export default function Home() {
+const  submitAction =  async (formData:FormData) => {
+  'use server';
+  console.log(formData)
+  f();  
+}
+const f = () => {
+  console.log('hello world')
+  console.log(localStorage)
+
+}
   return (
    <>
       <h1>home page</h1>
-      
+      <form action={submitAction}>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+        <button>Submit</button>
+      </form>
    </>
   );
 }
